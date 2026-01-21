72bad9713113907448f32201cb0a8b76657cbffe
Quantum Trotter <sirius_2003@mail.ru>
2026-01-21 16:04:25 +0300

    add Api and Dto

--- app/src/main/java/com/sinful/weatherapp/presentation/MainActivity.kt
+++ app/src/main/java/com/sinful/weatherapp/presentation/MainActivity.kt
@@ -1,15 +1,37 @@
 package com.sinful.weatherapp.presentation
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.lifecycle.lifecycleScope
+import com.sinful.weatherapp.data.network.api.ApiFactory
+import com.sinful.weatherapp.data.network.api.ApiService
 import com.sinful.weatherapp.presentation.ui.theme.WeatherAppTheme
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.async
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
+        val apiService = ApiFactory.apiService
+        lifecycleScope.launch {
+            val result = withContext(Dispatchers.IO) {
+                val current = apiService.loadCurrentWeather("London")
+                val forecast = apiService.loadForecast("London")
+                val cities = apiService.searchCity("London")
+                Triple(current, forecast, cities)
+            }
+
+            Log.d("MainActivity", result.toString())
+        }
+
         setContent {
             WeatherAppTheme {
 
